c27af309c3a2fada55a529e7ff34a172ffa96ae6
Nguyen Thanh Cong <cong.nguyen@beu.social>
2022-05-21 17:11:54 +0700

    rename logger

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -17,7 +17,7 @@ import (
 )
 
 func main() {
-	newLogger := logger.New(
+	dbLogger := logger.New(
 		log.New(os.Stdout, "\r\n", log.LstdFlags),
 		logger.Config{
 			SlowThreshold:              time.Second,
@@ -28,7 +28,7 @@ func main() {
 	)
 	dsn := "wager:123456@tcp(127.0.0.1:3306)/wager?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
-		Logger:                                   newLogger,
+		Logger:                                   dbLogger,
 	})
 	if err != nil {
 		fmt.Println(err.Error())
